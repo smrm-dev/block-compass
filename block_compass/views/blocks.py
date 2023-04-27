6cc69acb1681a9c3414ce85dfec07ae7d22119a8
smrm-dev <smrmdev@gmail.com>
2023-04-27 13:06:55 +0330

    Implement `Blocks` view `get` function

--- block_compass/views/blocks.py
+++ block_compass/views/blocks.py
@@ -6,4 +6,29 @@ from traceback import print_exc
 
 class Blocks(MethodView):
     def get(self):
-        return 'Ok', 200
+
+        try: 
+            params = request.args.to_dict()
+            timestamp = params['timestamp']
+            chain_ids = params.get('chain_ids')
+
+            if chain_ids is None:
+                ## TODO: should get all supported chains
+                chain_ids = [1,2,3]
+            else:
+                chain_ids = json.loads(chain_ids)
+
+
+            blocks = {}
+            for chain_id in chain_ids:
+                ## TODO: should query db
+                blocks[chain_id] = get_block_by_timestamp(timestamp, chain_id)
+
+            return jsonify(success=True, blocks=blocks), 200
+
+        except Exception as e:
+            print_exc()
+            return jsonify(success=False, message='Bad Request!'), 400
+
+def get_block_by_timestamp(timestamp, chain_id):
+    return timestamp
