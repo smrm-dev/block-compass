6ae2e1cbb4882f74b7960a1487c1668698b1a27a
smrm-dev <smrmdev@gmail.com>
2023-05-06 11:10:22 +0330

    Improve `SyncThread` & save `last_synced_block`

--- block_compass/utils/sync_thread.py
+++ block_compass/utils/sync_thread.py
@@ -10,7 +10,7 @@ from web3 import Web3
 from ..db import (
     get_chains,
     get_monitor_logs,
-    get_sync_logs,
+    get_last_synced_block,
     insert_block,
     save_monitor_log,
     save_sync_log,
@@ -24,34 +24,26 @@ class SyncThread(Thread):
         self.last_synced_block = None
         Thread.__init__(self, name=name)
 
-    def find_gaps(monitor_logs):
+    def __find_gaps(self, monitor_logs):
         gaps = []
         for sequent_logs in zip(monitor_logs[:-1], monitor_logs[1:]):
-            gaps.append((sequent_logs[0]['toBlock'] + 1, sequent_logs[1]['toBlock'] - sequent_logs[1]['numBlocks'] + 1, sequent_logs[1]['_id']))    
+            gaps.append((sequent_logs[0]['toBlock'] + 1, sequent_logs[1]['toBlock'] - sequent_logs[1]['numBlocks'] + 1))    
         
         return gaps
 
-
-    def sync_to_block(chain, start, end):
+    def __sync_to_block(self, chain, start, end):
         w3 = Web3(Web3.HTTPProvider(chain['rpc']))
 
         for block_number in range(start, end):
             block = w3.eth.get_block(block_number)
             insert_block(block, chain['id'])
             save_sync_log(block_number, chain['id'])
+            self.last_synced_block = block_number
 
-
-    def sync_chain(app, chain):
+    def __sync_chain(self, app, chain):
         with app.app_context():
-
-            sync_log = get_sync_logs(chain['id'])
-
-            last_synced_block = -1
-            if sync_log:
-                start = sync_log['toBlock'] + 1
-                last_synced_block = sync_log['toBlock']
-            else:
-                start = 0
+            last_synced_block = get_last_synced_block(chain["id"])
+            start = last_synced_block + 1
 
             monitor_logs = get_monitor_logs(last_synced_block, chain['id'])
 
@@ -59,15 +51,15 @@ class SyncThread(Thread):
 
             if monitor_logs == []:
                 end = w3.eth.get_block_number()
-                sync_to_block(chain, start, end + 1)
+                self.__sync_to_block(chain, start, end + 1)
             else:
                 first_log = monitor_logs[0]
                 first_gap_end = first_log['toBlock'] - first_log['numBlocks'] + 1
                 gaps = [(start, first_gap_end)]
-                gaps += find_gaps(monitor_logs)
-                print(chain['name'], gaps)
+                gaps += self.__find_gaps(monitor_logs)
+                print(chain["name"], gaps, '\n')
                 for gap in gaps:
-                    sync_to_block(chain, start=gap[0], end=gap[1])
+                    self.__sync_to_block(chain, start=gap[0], end=gap[1])
                 save_sync_log(monitor_logs[-1]['toBlock'], chain['id'])
 
             print(f'{chain["name"]} synced!')
