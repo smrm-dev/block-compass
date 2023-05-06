5a0d98f187b7dbe25f44df6bb04d27aa916a1254
smrm-dev <smrmdev@gmail.com>
2023-05-06 14:43:24 +0330

    Use `MonitorThread` in `monitor` command

--- block_compass/blueprints/blocks.py
+++ block_compass/blueprints/blocks.py
@@ -11,7 +11,7 @@ from flask import (
 
 
 from ..views import Blocks
-from ..utils import SyncThread
+from ..utils import SyncThread, MonitorThread
 from ..db import (
      get_chains,
      insert_block,
@@ -42,37 +42,12 @@ def sync():
         sleep(1) 
 
 
-def monitor_chain(app, chain):
-    with app.app_context():
-        start_time = datetime.now()
-        w3 = Web3(Web3.HTTPProvider(chain['rpc']))
-        
-        latest_block = w3.eth.get_block_number()
-        latest_monitored_block = latest_block - 1
-        while True:
-            new_blocks = [block_number for block_number in range(latest_monitored_block + 1, latest_block + 1)]
-            for block_number in new_blocks:
-                while True:
-                    try:
-                        block = w3.eth.get_block(block_number)
-                        insert_block(block, chain['id'])
-                        save_monitor_log(start_time, block['number'], chain['id'])
-                        latest_monitored_block = block['number']
-                        break
-                    except:
-                        print('********************', chain['name'], '********************')
-                        print_exc()
-                        sleep(1)
-            sleep(chain['blockTime'])
-            latest_block = w3.eth.get_block_number()
-
-
 @blocks_blueprint.cli.command('monitor')
 def monitor():
     chains = get_chains()
     threads = []
     for chain in chains:
-        thread = Thread(target=monitor_chain, args=(current_app._get_current_object(), chain,), name=chain['name']) 
+        thread = MonitorThread(current_app._get_current_object(), chain, chain['name'])
         thread.start()
         threads.append(thread)
     
@@ -82,6 +57,6 @@ def monitor():
     prettify = lambda cond, text : GREEN + text + ENDC if cond else RED + text + ENDC 
 
     while True: 
-        print('Monitoring chains: ', *[prettify(thread.is_alive(), thread.name) for thread in threads], end='\r') 
+        print('Monitoring chains: ', *[prettify(thread.is_alive(), f'{thread.name}({thread.latest_monitored_block})') for thread in threads], end='\r') 
         sleep(1) 
     
