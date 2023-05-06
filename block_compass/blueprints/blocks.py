804068e9029b6ab726aa5e5f70218381a8af4693
smrm-dev <smrmdev@gmail.com>
2023-05-06 11:34:57 +0330

    `monitor` using `get_block_number`

--- block_compass/blueprints/blocks.py
+++ block_compass/blueprints/blocks.py
@@ -46,21 +46,25 @@ def monitor_chain(app, chain):
     with app.app_context():
         start_time = datetime.now()
         w3 = Web3(Web3.HTTPProvider(chain['rpc']))
-        new_block_filter = w3.eth.filter('latest')
+        
+        latest_block = w3.eth.get_block_number()
+        latest_monitored_block = latest_block - 1
         while True:
-            new_blocks = new_block_filter.get_new_entries()
-            for block_hash in new_blocks:
+            new_blocks = [block_number for block_number in range(latest_monitored_block + 1, latest_block + 1)]
+            for block_number in new_blocks:
                 while True:
                     try:
-                        block = w3.eth.get_block(block_hash)
+                        block = w3.eth.get_block(block_number)
                         insert_block(block, chain['id'])
                         save_monitor_log(start_time, block['number'], chain['id'])
+                        latest_monitored_block = block['number']
                         break
                     except:
                         print('********************', chain['name'], '********************')
                         print_exc()
                         sleep(1)
             sleep(chain['blockTime'])
+            latest_block = w3.eth.get_block_number()
 
 
 @blocks_blueprint.cli.command('monitor')
