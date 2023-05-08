f33338ee042059378b46a166c033635e55ea2df2
smrm-dev <smrmdev@gmail.com>
2023-05-08 13:02:39 +0330

    Implement chunking and parallel processing

--- block_compass/utils/sync_thread.py
+++ block_compass/utils/sync_thread.py
@@ -18,20 +18,29 @@ class SyncThread(Thread):
         self.app = app
         self.chain = chain
         self.last_synced_block = None
+        self.threads = []
         Thread.__init__(self, name=name)
 
     def __find_gaps(self, start, monitor_logs, chain):
         if monitor_logs == []:
             w3 = Web3(Web3.HTTPProvider(chain['rpc']))
             end = w3.eth.get_block_number()
-            return [(start, end + 1)]
+            return [(start, end + 1, end - start + 1)]
         else:
             first_log = monitor_logs[0]
             first_gap_end = first_log['toBlock'] - first_log['numBlocks'] + 1
-            gaps = [(start, first_gap_end)]
+
+            gaps = []
+            if start < first_gap_end - 1:
+                gaps += [(start, first_gap_end, first_gap_end - start)]
 
             for sequent_logs in zip(monitor_logs[:-1], monitor_logs[1:]):
-                gaps.append((sequent_logs[0]['toBlock'] + 1, sequent_logs[1]['toBlock'] - sequent_logs[1]['numBlocks'] + 1))    
+                gap_start = sequent_logs[0]['toBlock'] + 1
+                gap_end = sequent_logs[1]['toBlock'] - sequent_logs[1]['numBlocks'] + 1
+                gap_length = gap_end - gap_start
+                if gap_length == 0:
+                    continue
+                gaps.append((gap_start, gap_end, gap_end - gap_start))    
         
         return gaps
 
@@ -43,6 +52,59 @@ class SyncThread(Thread):
             insert_block(block, chain['id'])
             save_sync_log(block_number, chain['id'])
             self.last_synced_block = block_number
+    
+    def __sync_chunk(self, rpc, chunk, chain):
+        #TODO: implement
+        print(chain['name'], rpc, chunk)
+
+    def __sync_blocks_in_chunks(self, gaps, chain):
+        def split_gap(gap, chunk_free_space):
+            split_point = gap[0] + chunk_free_space
+            return [(gap[0], split_point, chunk_free_space), (split_point, gap[1], gap[2] - chunk_free_space)] 
+        
+        num_blocks_to_sync = sum([gap[2] for gap in gaps])
+
+        # TODO: should fetch chain rpcs 
+        num_chain_rpcs = 3 
+        chain_rpcs = ['1', '2', '3']
+
+        average_chunk_size = (num_blocks_to_sync // num_chain_rpcs) + 1
+
+        chunks = {api_key: [] for api_key in chain_rpcs}
+        for chain_rpc in chain_rpcs:
+            chunk_gaps = []
+            chunk_size = 0
+
+            last_index = -1 
+            for index, gap in enumerate(gaps):
+                gap_size = gap[2]
+
+                if gap_size + chunk_size <= average_chunk_size:
+
+                    chunk_gaps.append(gap)
+                    chunk_size += gap_size
+                    last_index += 1
+                    if chunk_size == average_chunk_size:
+                        break
+                    continue
+
+                (gap_0, gap_1) = split_gap(gap, average_chunk_size - chunk_size) 
+                gaps[index] = gap_1
+                chunk_gaps.append(gap_0)
+                break
+
+            chunks[chain_rpc] = { 'gaps':chunk_gaps, 'chainId': chain['id'] }
+            gaps = gaps[last_index + 1:]
+
+        #TODO: update sync log 
+        for chain_rpc in chain_rpcs:
+            thread = Thread(target=self.__sync_chunk, args=(chain_rpc, chunks[chain_rpc], chain,))
+            thread.start()
+            self.threads.append(thread)
+
+        for thread in self.threads:
+            thread.join()
+
 
     def __sync_chain(self, app, chain):
 
@@ -61,7 +123,11 @@ class SyncThread(Thread):
                 gaps += sync_log_gaps
 
             gaps += self.__find_gaps(start, monitor_logs, chain)
+            if gaps == []:
+                print(f'{chain["name"]} has already synced!')
+                return
 
+            print(chain['name'], gaps)
             self.__sync_blocks_in_chunks(gaps, chain)
 
             print(f'{chain["name"]} synced!')
